cc4d9a37ffd8e1310d7d19410d6646d81a3405d4
faiz832 <faqodefazz@gmail.com>
2025-04-23 15:49:52 +0700

    add splash

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@
 
 import CurrentActivity from "./components/CurrentActivity";
 import CurrentTime from "./components/CurrentTime";
-import Squares from "./components/Squares";
+import Squares from "./components/Squares/Squares";
+import SplashCursor from "./components/SplashCursor/SplashCursor";
 
 export default function Home() {
   return (
     <main className="relative min-h-screen flex flex-col items-center justify-center p-4">
+      <SplashCursor />
       <div className="absolute top-0 left-0 right-0 bottom-0 w-full h-full">
         <Squares
           speed={0.3}
