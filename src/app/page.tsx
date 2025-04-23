6440d6e36aa7034d8834ff8f3729fd2e11766fdd
faiz832 <faqodefazz@gmail.com>
2025-04-23 08:50:48 +0700

    fix current time hydration mismatch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import CurrentActivity from "./components/CurrentActivity";
 import CurrentTime from "./components/CurrentTime";
 
@@ -6,7 +8,6 @@ export default function Home() {
     <main className="min-h-screen flex flex-col items-center justify-center p-4">
       <CurrentTime />
       <CurrentActivity />
-      {/* Tombol Modal + Tema nanti di sini */}
     </main>
   );
 }
